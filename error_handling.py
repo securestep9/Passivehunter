192d8265da6b0b6082fe78eb0c7791c131bc4494
Devansh Batham <devanshbatham009@gmail.com>
2020-02-26 15:24:51 +0530

    Update error_handling.py

--- error_handling.py
+++ error_handling.py
@@ -10,21 +10,21 @@ def error_handler(url):
         res = requests.get(url,timeout=30)
     except requests.ConnectionError as e:
         print("\u001b[31;1mOOPS!! Connection Error. Make sure you are connected to Internet.\u001b[0m")
-        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : [todo]\u001b[0m")
+        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : https://github.com/devanshbatham/Passivehunter/issues\u001b[0m")
         print("\u001b[0m")
         sys.exit()
     except requests.Timeout as e:
         print("\u001b[31;1mOOPS!! Timeout Error\u001b[0m")
-        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : [todo]\u001b[0m")
+        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : https://github.com/devanshbatham/Passivehunter/issues\u001b[0m")
         print("\u001b[0m")
         sys.exit()
     except requests.RequestException as e:
         print("\u001b[31;1mOOPS!! General Error\u001b[0m")
-        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : [todo]\u001b[0m")
+        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : https://github.com/devanshbatham/Passivehunter/issues\u001b[0m")
         print("\u001b[0m")
         sys.exit()
     except KeyboardInterrupt:
         print("\u001b[31;1mSomeone closed the program\u001b[0m")
-        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : [todo]\u001b[0m")
+        print("\u001b[31;1mIf you think this is a bug or unintentional behaviour. Report here : https://github.com/devanshbatham/Passivehunter/issues\u001b[0m")
         print("\u001b[0m")
         sys.exit()
